a01728c6f52b5920a86be18887584fe62839f4f3
Rodrigo Von-Grapp Freitas <rodrigo.freitas.rrod@gmail.com>
2014-06-03 16:26:41 -0300

    Initial Commit

---
+++ ProjetoQQISSOFinal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProjetoQQISSOFinal
+//
+//  Created by Rodrigo Von-Grapp Freitas on 03/06/14.
+//  Copyright (c) 2014 Rodrigo Freitas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProjetoQQISSOFinal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjetoQQISSOFinal
+//
+//  Created by Rodrigo Von-Grapp Freitas on 03/06/14.
+//  Copyright (c) 2014 Rodrigo Freitas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
